412a824cc25f2cbea11769ced1bd7f3e21a8ede1
Braeden L <braedenlacombe@u.boisestate.edu>
2023-04-13 20:57:34 -0600

    Initial File Upload

---
+++ TMSimulator.java
@@ -0,0 +1,15 @@
+public class TMSimulator {
+    //Main class to create and manage the Turing Machine
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    //Just creating space
+    public void function()
+    {
+
+    }
+}
