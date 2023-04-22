6155c5709dc54a45ab119e0db30fdd799ff9fb48
Andrew Lackey <andrewlackey@u.boisestate.edu>
2023-04-22 17:42:29 -0600

    Changed the gets to use iterator

--- tm/TMSimulator.java
+++ tm/TMSimulator.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class TMSimulator {
@@ -13,6 +14,7 @@ public class TMSimulator {
 
         //Handle the tape using a doubly linked list, using LinkedList java class
         LinkedList<Integer> tape = new LinkedList<Integer>();
+        
         //Main position tracker for the tape
 
         //Other instance variables
@@ -167,8 +169,12 @@ public class TMSimulator {
         
         //TM halts upon reaching accept no matter what, so why not just track where our state is and if we reach the end (biggest) state
         //Can add a check inside of the machine in case we run out of tape
+
         int testTracker = 0;
         int previousState;
+        ListIterator<Integer> headIter = tape.listIterator(0);
+        int currentElement = headIter.next();
+        int lastMove = 1; // 0 represents the last move being a previous and a 1 represents the last move being a next
         while(currentState != totalStates-1)
         {
             //Print out each position in tape visited in the machine
@@ -180,48 +186,73 @@ public class TMSimulator {
             //     System.out.println("made it back");
             // }
             testTracker++;
-            if(TM.get(currentState).isTransition(tape.get(head)))
+            if(TM.get(currentState).isTransition(currentElement))
             {
                 //Move the tape
-                if(TM.get(currentState).getTapeDirection(tape.get(head)).equals("L"))
+                if(TM.get(currentState).getTapeDirection(currentElement).equals("L"))
                 {
                     // System.out.print(tape.get(head));
 
                     previousState = currentState;
                     //Checked if using a previous state counter was issue!!!
-                    currentState = TM.get(currentState).getNextState(tape.get(head));
-                    tape.set(head, TM.get(previousState).getWriteValue(tape.get(head)));
+                    currentState = TM.get(currentState).getNextState(currentElement);
+                    headIter.set(TM.get(previousState).getWriteValue(currentElement));
                     if(TM.get(currentState).isAcceptState())
                     {
                         //Program has reached the accepted state and rest of the tape and string can be ignored 
                         break;
                     }
-                    //I THINK THIS ADD IS WRONG (changed to indexOf(head))
                     if(head == 0) {
-                        tape.addFirst(0);
+                        //if last call was a next, need to make sure iter is before the first element in list before adding, so call a previous
+                        if(lastMove == 1){
+                            currentElement = headIter.previous();//techincally this shouldn't change the value of currentElement (use for debugging)
+                            lastMove = 0; //since we just went previous, need to update the last move variable
+                        }
+                        headIter.add(0);
+                        // tape.addFirst(0);
                     }
-                    else{//I think this head-- needs checking where it is to decide if to move it, mainly when adding new 0
-                    head--;
+                    else{
+                        head--; 
+                        //since we are moving back by one, we need to call prev on iter once no matter what
+                        currentElement = headIter.previous();
+                        //but if the last move used to find currentElement, was a next then we need to call previous twice to update currentElement correctly
+                        if(lastMove == 1) {
+                            currentElement = headIter.previous();
+                        }
+                        lastMove = 0; //update that we moved back wether if statement was executed or not
                     }
                 }
-                else if(TM.get(currentState).getTapeDirection(tape.get(head)).equals("R"))
+                else if(TM.get(currentState).getTapeDirection(currentElement).equals("R"))
                 {
                     // System.out.print(tape.get(head));
 
                     previousState = currentState;
                     //CHECKED IF USING PREVIOUS STATE COUNTER WAS ISSUE !!!
-                    currentState = TM.get(currentState).getNextState(tape.get(head));
+                    currentState = TM.get(currentState).getNextState(currentElement);
                     
-                    tape.set(head, TM.get(previousState).getWriteValue(tape.get(head)));
+                    headIter.set(TM.get(previousState).getWriteValue(currentElement));
                     if(TM.get(currentState).isAcceptState())
                     {
                         //Program has reached the accepted state and rest of the tape and string can be ignored 
                         break;
                     }
                     if(head == (tape.size() - 1)) {
-                        tape.addLast(0);
+                        //if last call was a previous, need to make sure iter is after the last element in list before adding, so call a next
+                        if(lastMove == 0){
+                            currentElement = headIter.next();//techincally this shouldn't change the value of currentElement (use for debugging)
+                            lastMove = 1; //since we just went next, need to update the last move variable
+                        }
+                        headIter.add(0);
+                        // tape.addLast(0);
                     }
                     head++;//since were adding a zero at the end we still need to shift the head to move into the new spot
+                    //since we are moving forward by one, we need to call next on iter once no matter what
+                    currentElement = headIter.next();
+                    //but if the last move used to find currentElement, was a previous then we need to call next twice to update currentElement correctly
+                    if(lastMove == 0) {
+                        currentElement = headIter.next();
+                    }
+                    lastMove = 1; //update that we moved forward wether if statement was executed or not
                 }
                 //Need to move the current state
                 //System.out.println(currentState + "and head is located at: " + head);
